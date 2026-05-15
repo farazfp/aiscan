4bd2e4b275329a9197cff015feb9ad1af8864f4e
farazfp <faraz.fp@gmail.com>
2026-05-14 17:39:04 -0700

    Quartz sync: May 14, 2026, 5:39 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "farazfp.github.io/aiscan",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
